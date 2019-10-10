4a6309db74257b1a03416d7d93039448ccf66378
Melike KILIC <melike180400@gmail.com>
2019-10-10 16:51:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,11 +2,15 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-def introduction(name=  "Maria")
-puts "Hi,my name is #{name}."
+#def introduction(name=  "Maria")
+#puts "Hi,my name is #{name}."
+#end
+#introduction
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
-introduction
-
+ 
+introduction("Maria")
 
 
 
