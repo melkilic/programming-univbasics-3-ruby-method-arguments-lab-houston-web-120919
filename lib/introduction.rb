f3a9a55e805433c46f5753c1f060cb3619de651c
Melike KILIC <melike180400@gmail.com>
2019-10-10 16:58:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -17,6 +17,13 @@ puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 introduction_with_language("Mel","Ruby")
 
+def introduction_with_language_optional(name,language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language_optional("Ashley","Java")
+introduction_with_language_optional("Ashley")
+  
+
 
 
 
